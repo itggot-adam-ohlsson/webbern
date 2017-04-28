7d947602c36f53ea9f6719f6708edb85c6287bc5
itggot-adam-ohlsson <adam.olsson@itggot.se>
2017-04-28 21:18:06 +0200

    Version 1, Header + Top layout klart!

---
+++ site/js/functions.js
@@ -0,0 +1,21 @@
+setInterval(function () {
+  header_move = $("body").scrollTop();
+  if (header_move > 0) {
+    $("header").addClass("header");
+    $(".header_content").addClass("header");
+    $(".logo").addClass("logo_mini");
+  }
+  else {
+    $("header").removeClass("header");
+    $(".header_content").removeClass("header");
+    $(".logo").removeClass("logo_mini");
+  }
+}, 100);
+
+$(".top").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 200);
+});
+
+$(window).scroll(function() {
+  $(".top_content").css("opacity", 1 - $(window).scrollTop() / 400);
+});
